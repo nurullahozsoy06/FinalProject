8fbeea383d040ae04df051c3506dc540b1ec2f7a
Nurullah Özsoy <sadeceozsoy@hotmail.com>
2026-05-12 08:59:13 +0300

    12 mayıs

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //atribute
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+
+        public List<Product> Get()
+        {
+
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+        }
+    }
+}
