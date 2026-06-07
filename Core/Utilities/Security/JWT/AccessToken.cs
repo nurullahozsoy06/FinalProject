aa25a0cec9e4e89c62b6034954ae2671369961f1
Nurullah Özsoy <sadeceozsoy@hotmail.com>
2026-06-07 17:42:47 +0300

    7 haziran

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class AccessToken
+    {
+        public string Token { get; set; } //token değeri
+        public DateTime Expiration { get; set; } //tokenın ne zaman geçersiz olacağını belirten tarih
+
+    }
+}
